4b0a8622695b0fd45332137b637eeb9b4ad0ebb1
alexhfrey <alex.h.frey@gmail.com>
2012-07-24 09:59:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrannysKitchen::Application.config.secret_token = '8d38dda1553dff27f78afbbf533e4a1032b80b06b8dd4cde8559fdbe1a946fc990cedcf556bd8d9a6edeb45790e3a06def1614f541c788bbcb8e572e2637cfd1'
