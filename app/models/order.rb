47634dea5e610d7ba4e3a43e6a279c968f0aac40
Alex H Frey <alex.h.frey@gmail.com>
2012-08-20 13:58:06 -0700

    ';lkj

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,9 @@ class Order < ActiveRecord::Base
   belongs_to :customer
   belongs_to :week
   has_many :extra_orders
-  accepts_nested_attributes_for :extra_orders, reject_if: :all_blank, allow_destroy:true
+  accepts_nested_attributes_for :extra_orders, :reject_if => lambda { |a| a[:extra_id].blank? }
+  
+ 
   
   def self.to_csv(orders)
 	CSV.generate do |csv|
--- app/models/week.rb
+++ app/models/week.rb
@@ -1,7 +1,7 @@
 class Week < ActiveRecord::Base
 has_many :orders
 has_many :extras
-accepts_nested_attributes_for :extras
+accepts_nested_attributes_for :extras, :reject_if => lambda { |a| a[:name].blank? }
 
 def end_date
 	start_date + 3.days
