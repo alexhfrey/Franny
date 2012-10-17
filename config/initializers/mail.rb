6235163e4712a9c33e22d79e7240f2035b1cb668
alexhfrey <alex.h.frey@gmail.com>
2012-10-16 17:42:56 -0700

    sendgrid:

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => 'frannyskitchen',
-  :password       => '100chickens',
-  :domain         => 'heroku.com'
+  :user_name      => 'franny',
+  :password       => '8Chickens',
+  :domain         => 'frannyskitchensf.com'
 }
 ActionMailer::Base.delivery_method = :smtp
